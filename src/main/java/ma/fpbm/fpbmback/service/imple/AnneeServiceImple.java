4330800978a988d5f494909e005595a742e5c265
aymaneharkati <aymaneharkati@gmail.com>
2022-07-04 23:10:20 +0100

    feat(fpbm) : Rest controller for Annee beans

---
+++ src/main/java/ma/fpbm/fpbmback/service/imple/AnneeServiceImple.java
@@ -0,0 +1,44 @@
+package ma.fpbm.fpbmback.service.imple;
+
+import ma.fpbm.fpbmback.beans.Annee;
+import ma.fpbm.fpbmback.beans.CoursTdTp;
+import ma.fpbm.fpbmback.repository.AnneeRepository;
+import ma.fpbm.fpbmback.service.facade.AnneeService;
+import ma.fpbm.fpbmback.service.facade.CoursTdTpService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class AnneeServiceImple implements AnneeService {
+    @Autowired
+    private AnneeRepository anneeRepository;
+
+    @Override
+    public List<Annee> findAll() {
+        return anneeRepository.findAll();
+    }
+
+    @Override
+    public Optional<Annee> findById(Long id) {
+        return anneeRepository.findById(id);
+    }
+
+    public Annee save(Annee annee) {
+        // validate the input data :
+            return anneeRepository.save(annee);
+    }
+
+    @Override
+    public String deleteByCode(Long id) {
+       anneeRepository.deleteById(id);
+       return "Deleted";
+    }
+
+    @Override
+    public Annee update(Annee annee) {
+        return anneeRepository.save(annee);
+    }
+}
