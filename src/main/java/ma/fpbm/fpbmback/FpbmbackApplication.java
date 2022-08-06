ad5a4488b02c9f6876fd4b96a93aabe0cb50abcc
aminebatsi <batsiamine@gmail.com>
2022-08-06 12:01:02 +0100

    Spring sercurity Authentication system

--- src/main/java/ma/fpbm/fpbmback/FpbmbackApplication.java
+++ src/main/java/ma/fpbm/fpbmback/FpbmbackApplication.java
@@ -1,7 +1,16 @@
 package ma.fpbm.fpbmback;
 
+import ma.fpbm.fpbmback.security.Beans.AppRole;
+import ma.fpbm.fpbmback.security.Beans.AppUser;
+import ma.fpbm.fpbmback.security.services.AccountService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class FpbmbackApplication {
@@ -9,5 +18,32 @@ public class FpbmbackApplication {
     public static void main(String[] args) {
         SpringApplication.run(FpbmbackApplication.class, args);
     }
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+    /*
+    @Bean
+    CommandLineRunner start(AccountService accountService){
+        return args -> {
+            accountService.addNewRol(new AppRole(null,"USER"));
+            accountService.addNewRol(new AppRole(null,"ADMIN"));
+            //accountService.addNewRol(new AppRole(null,"MANAGER"));
+            //accountService.addNewRol(new AppRole(null,"SCOLARITE"));
+
+            accountService.addNewUser(new AppUser(null,"user1","123",new ArrayList<>()));
+            accountService.addNewUser(new AppUser(null,"admin","123",new ArrayList<>()));
+            accountService.addNewUser(new AppUser(null,"user2","123",new ArrayList<>()));
+            //accountService.addNewUser(new AppUser(null,"user3","123",new ArrayList<>()));
+            //accountService.addNewUser(new AppUser(null,"user4","123",new ArrayList<>()));
+
+            accountService.addRoleToUser("user1","USER");
+            accountService.addRoleToUser("admin","ADMIN");
+            accountService.addRoleToUser("user2","USER");
+
+
+        };
+    }*/
 
 }
